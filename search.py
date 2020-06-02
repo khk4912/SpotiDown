8179cf6bcd8aceee59fc9b6ea8ccc66e03e409bc
khk4912 <30457148+khk4912@users.noreply.github.com>
2020-06-03 01:51:45 +0900

    Base

---
+++ search.py
@@ -0,0 +1,7 @@
+from spotipy import Spotify
+from pytube import YouTube
+
+
+class Search:
+    """ 검색 관련 클래스 """
+    def __init__(self, type: str, llama: str):
---
+++ utils.py
@@ -0,0 +1,5 @@
+import CONFIG
+
+
+class Utils:
+    """ 모듈에 사용되는 유틸리티 메소드 """
